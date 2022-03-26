5468876b32c8d573540c0deace07efbd9cc69f38
Jacek Trociński <jack.trocinski@gmail.com>
2022-03-27 00:54:42 +0100

    Change BAN address to YOUR_BANANO_ADDRESS.

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 ./bin/linux/nano-work-server -c $(nproc) -l 127.0.0.1:7000 &
-python3.7 bpow_client.py --payout ban_1z1ezq8s1c4f95aa9m5uuysiqo69knrtcn8scwm6yewnf1sycdhdw4f1tsqq --work any
+python3.7 bpow_client.py --payout YOUR_BANANO_ADDRESS --work any
